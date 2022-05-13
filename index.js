f7db0eab0b4531d590ef36e17854bfcf35aa5fa4
Safa Rizwan <103735300+Safa-Rizwan@users.noreply.github.com>
2022-05-14 00:34:22 +0500

    1st comment

---
+++ index.js
@@ -0,0 +1,11 @@
+// 1. ways to print in JS---------
+
+//console.log("hello world");
+//alert("me");
+//document.write("hii Safa");
+
+// 2. JS Console API -------------
+//console.warn("Warning hai");
+//console.warn("Warning hai", 4 + 5, "multiple arguments allowed")
+        //console.error("Error hai");
+        //console.assert(6==4);
