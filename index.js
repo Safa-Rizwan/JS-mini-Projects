168f3fbee90f6003312ea752816e7758cff82ea4
Safa Rizwan <103735300+Safa-Rizwan@users.noreply.github.com>
2022-05-14 11:01:37 +0500

    Data Types Completed

--- index.js
+++ index.js
@@ -5,7 +5,37 @@
 //document.write("hii Safa");
 
 // 2. JS Console API -------------
+
 //console.warn("Warning hai");
-//console.warn("Warning hai", 4 + 5, "multiple arguments allowed")
-        //console.error("Error hai");
-        //console.assert(6==4);
+//console.warn("Warning hai", 4 + 5",multiple arguments allowed")
+//console.error("Error hai");
+//console.assert(6==4);
+
+// 3. Js Variables
+// what are variables? - Containers to store data values
+var number1=37
+var number2=98
+console.log(number1+number2);
+
+/* 4. Data Types-------------------*/
+// integers
+var num1=45;
+var num2=89;
+// strings
+var str="Hello World";
+//boolean
+var bool=True;
+//object
+var marks={
+    urdu:100,
+    maths:59} //key value pairs
+
+
+/*  At a very high level, there are 2 data types
+1- primitive data types: undefined, null, number, String, boolean, symbol
+
+2- Reference data types: array, object */
+
+var arr=[1,2,3,4,5]; //multiples data types allowed in it
+
+
