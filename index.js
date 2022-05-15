7aa76582fff6da79d063ecdbaff0643bd13e8a6c
Safa Rizwan <103735300+Safa-Rizwan@users.noreply.github.com>
2022-05-15 11:18:31 +0500

    DOM completed

--- index.js
+++ index.js
@@ -1,75 +1,114 @@
-// 1. ways to print in JS---------
+// // 1. ways to print in JS---------
 
-//console.log("hello world");
-//alert("me");
-//document.write("hii Safa");
+// //console.log("hello world");
+// //alert("me");
+// //document.write("hii Safa");
 
-// 2. JS Console API -------------
+// // 2. JS Console API -------------
 
-//console.warn("Warning hai");
-//console.warn("Warning hai", 4 + 5",multiple arguments allowed")
-//console.error("Error hai");
-//console.assert(6==4);
+// //console.warn("Warning hai");
+// //console.warn("Warning hai", 4 + 5",multiple arguments allowed")
+// //console.error("Error hai");
+// //console.assert(6==4);
 
-// 3. Js Variables
-// what are variables? - Containers to store data values
-var number1=37
-var number2=98
-console.log(number1+number2);
+// // 3. Js Variables
+// // what are variables? - Containers to store data values
+// /*var number1=37
+// var number2=98
+// console.log(number1+number2);
 
-/* 4. Data Types-------------------*/
-// integers
-var num1=45;
-var num2=89;
-// strings
-var str="Hello World";
-//boolean
-var bool=True;
-//object
-var marks={
-    urdu:100,
-    maths:59} //key value pairs
+// /* 4. Data Types-------------------*/
+// // integers
+// var num1=45;
+// var num2=89;
+// // strings
+// var str="Hello World";
+// //boolean
+// var bool=True;
+// //object
+// var marks={
+//     urdu:100,
+//     maths:59} //key value pairs
 
 
-/*  At a very high level, there are 2 data types
-1- primitive data types: undefined, null, number, String, boolean, symbol
+// /*  At a very high level, there are 2 data types
+// 1- primitive data types: undefined, null, number, String, boolean, symbol
 
-2- Reference data types: array, object */
+// 2- Reference data types: array, object */
 
-var arr=[1,2,3,4,5]; //multiples data types allowed in it
+// var arr=[1,2,3,4,5]; //multiples data types allowed in it
 
-//  operators in JS-----------------------
-// Arithmetic Operators
-console.log(num1+num2);
-console.log(num1-num2);
-console.log(num1*num2);
-console.log(num1/num2);
+// //  operators in JS-----------------------
+// // Arithmetic Operators
+// console.log(num1+num2);
+// console.log(num1-num2);
+// console.log(num1*num2);
+// console.log(num1/num2);
 
-//Assignment Operators
-var c=num1;
+// //Assignment Operators
+// var c=num1;
 
-// Comparison Operators
-console.log(num1==num2);
-console.log(num1>=num2);
-console.log(num1<=num2);
-console.log(num1<num2);
-console.log(num1>num2);
+// // Comparison Operators
+// console.log(num1==num2);
+// console.log(num1>=num2);
+// console.log(num1<=num2);
+// console.log(num1<num2);
+// console.log(num1>num2);
 
-//logical operators
-// And 
-console.log(true&&true);
+// //logical operators
+// // And 
+// console.log(true&&true);
 
-//OR
-console.log(true||false);
+// //OR
+// console.log(true||false);
 
-//not
-console.log(!false);
+// //not
+// console.log(!false);
 
-// functions--------------------
-// DRY Principle  = Do not repeat yourself
-function avg(a,b){
-    return (a+b)/2;
-}
-c1= avg(3,4);
-console.log(c1);
+// // functions--------------------
+// // DRY Principle  = Do not repeat yourself
+// function avg(a,b){
+//     return (a+b)/2;
+// }
+// c1= avg(3,4);
+// console.log(c1);
+
+// // If Else-----
+// var age = 7;
+// //Single if else
+// if(age>3){
+//     console.log("you can ride");
+// }
+// else{
+//     console.log("you cant ride");
+// }
+// // IF else ladder ---> multiple else-if
+
+// // array methods 
+// // strings methods
+// // Date 
+
+// let myDate = new Date();
+// console.log(myDate);
+// console.log(myDate.getTime());
+// console.log(myDate.getFullYear());
+// console.log(myDate.getDay());
+
+// DOM Manupulation-------------
+// document.getElementById('click').style.border= '4px solid red';
+// let a = document.getElementById('click');
+// console.log(a);
+// console.log(a.innerHTML);
+// console.log(a.innerText)
+// tn = document.getElementsByTagName('div');
+// createdElement = document.createElement('p');
+// createdElement.innerText = "this is created para";
+// tn[0].appendChild(createdElement);
+// tn[0].classList.add('text');
+
+// Query Selector--------------
+u=document.querySelector('.continer');
+console.log(u);
+
+
 
